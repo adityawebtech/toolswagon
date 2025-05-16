f8880ef79a438df9a62d5953a0d5a566f964082b
adityawebtech <globalwebkraftofficial@gmail.com>
2025-05-16 07:16:14 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
+// Load header and footer dynamically
 document.addEventListener("DOMContentLoaded", () => {
-  const basePath = "/toolswagon"; // your GitHub repo name
-
-  fetch(`${basePath}/components/header.html`)
+  fetch("/components/header.html")
     .then(res => res.text())
     .then(data => {
       document.getElementById("header-placeholder").innerHTML = data;
     });
 
-  fetch(`${basePath}/components/footer.html`)
+  fetch("/components/footer.html")
     .then(res => res.text())
     .then(data => {
       document.getElementById("footer-placeholder").innerHTML = data;
