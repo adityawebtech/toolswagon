07886f40ce8f64d1415c52a37024c96fbccee7b0
adityawebtech <globalwebkraftofficial@gmail.com>
2025-05-27 21:34:06 +0530

    Update star-rating.js

--- tools/js/star-rating.js
+++ tools/js/star-rating.js
@@ -3,14 +3,20 @@
 <script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore-compat.js"></script>
 
 <script>
+  // Firebase config
   const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyD5tK6AQqKkda5VOZfljvVX4zZfy_SoHX8",
+    authDomain: "toolswagon-ratings.firebaseapp.com",
+    projectId: "toolswagon-ratings",
+    storageBucket: "toolswagon-ratings.firebasestorage.app",
+    messagingSenderId: "96777512295",
+    appId: "1:96777512295:web:c08ebea6a9b835130ffbc2",
+    measurementId: "G-XFWQZD0NXB"
   };
+
+  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  // Initialize Firestore
   const db = firebase.firestore();
 </script>
