24f0c2ea44be2b8249d643e1ed08e1a73059cbf0
adityawebtech <globalwebkraftofficial@gmail.com>
2025-06-03 09:41:01 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+<!-- PushAlert -->
+<script type="text/javascript">
+        (function(d, t) {
+                var g = d.createElement(t),
+                s = d.getElementsByTagName(t)[0];
+                g.src = "https://cdn.pushalert.co/integrate_aa5185f5440830a483ba994933449000.js";
+                s.parentNode.insertBefore(g, s);
+        }(document, "script"));
+</script>
+<!-- End PushAlert -->
