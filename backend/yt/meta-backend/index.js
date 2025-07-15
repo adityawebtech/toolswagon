7d3b647f2fa5f7c47b4c012c0ccd6e52954eeece
adityawebtech <globalwebkraftofficial@gmail.com>
2025-07-16 00:45:26 +0530

    Update index.js

--- backend/yt/meta-backend/index.js
+++ backend/yt/meta-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+};
 
 const app = express();
 const PORT = process.env.PORT || 3000;
